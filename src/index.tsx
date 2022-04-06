e02bbd3478280237ae4a11389d97a6eaadd6de2b
alexeynikinitin <alexeynikinitin@yandex.by>
2022-04-06 23:05:42 +0300

    install eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
+
+import 'index.css';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './n1-main/m1-ui/App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import {store} from "./n1-main/m2-bll/a1-redux-store/store";
-import {HashRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+import App from 'n1-main/m1-ui/App';
+import { store } from 'n1-main/m2-bll/a1-redux-store/store';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
-    <HashRouter>
-      <Provider store={store}>
-        <App/>
-      </Provider>
-    </HashRouter>,
-  document.getElementById('root')
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
